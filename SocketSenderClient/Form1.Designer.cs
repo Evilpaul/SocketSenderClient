e438da840a8688ef724bf65b7b5f5680a3b9619d
DSUK Paul Young <p.young@denso-sales.co.uk>
2015-03-06 17:26:37 +0000

    initial commit

---
+++ SocketSenderClient/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace SocketSenderClient
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("NightMode: Off");
+			System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("NightMode: On");
+			this.OpenSocketButton = new System.Windows.Forms.Button();
+			this.CloseSocketButton = new System.Windows.Forms.Button();
+			this.ServerIpBox = new System.Windows.Forms.TextBox();
+			this.PortNoBox = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.OutputLog = new System.Windows.Forms.ListBox();
+			this.SendMsgButton = new System.Windows.Forms.Button();
+			this.MsgBox = new System.Windows.Forms.TextBox();
+			this.MessageList = new System.Windows.Forms.ListView();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.SuspendLayout();
+			// 
+			// OpenSocketButton
+			// 
+			this.OpenSocketButton.Location = new System.Drawing.Point(175, 6);
+			this.OpenSocketButton.Name = "OpenSocketButton";
+			this.OpenSocketButton.Size = new System.Drawing.Size(75, 46);
+			this.OpenSocketButton.TabIndex = 0;
+			this.OpenSocketButton.Text = "Open Socket";
+			this.OpenSocketButton.UseVisualStyleBackColor = true;
+			this.OpenSocketButton.Click += new System.EventHandler(this.OpenSocketButton_Click);
+			// 
+			// CloseSocketButton
+			// 
+			this.CloseSocketButton.Enabled = false;
+			this.CloseSocketButton.Location = new System.Drawing.Point(256, 6);
+			this.CloseSocketButton.Name = "CloseSocketButton";
+			this.CloseSocketButton.Size = new System.Drawing.Size(75, 46);
+			this.CloseSocketButton.TabIndex = 1;
+			this.CloseSocketButton.Text = "Close Socket";
+			this.CloseSocketButton.UseVisualStyleBackColor = true;
+			this.CloseSocketButton.Click += new System.EventHandler(this.CloseSocketButton_Click);
+			// 
+			// ServerIpBox
+			// 
+			this.ServerIpBox.Location = new System.Drawing.Point(69, 6);
+			this.ServerIpBox.Name = "ServerIpBox";
+			this.ServerIpBox.Size = new System.Drawing.Size(100, 20);
+			this.ServerIpBox.TabIndex = 2;
+			// 
+			// PortNoBox
+			// 
+			this.PortNoBox.Location = new System.Drawing.Point(69, 32);
+			this.PortNoBox.Name = "PortNoBox";
+			this.PortNoBox.Size = new System.Drawing.Size(100, 20);
+			this.PortNoBox.TabIndex = 3;
+			this.PortNoBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PortNoBox_KeyPress);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(51, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Server IP";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 35);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(46, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Port No.";
+			// 
+			// OutputLog
+			// 
+			this.OutputLog.FormattingEnabled = true;
+			this.OutputLog.HorizontalScrollbar = true;
+			this.OutputLog.Location = new System.Drawing.Point(12, 58);
+			this.OutputLog.Name = "OutputLog";
+			this.OutputLog.Size = new System.Drawing.Size(319, 212);
+			this.OutputLog.TabIndex = 6;
+			// 
+			// SendMsgButton
+			// 
+			this.SendMsgButton.Enabled = false;
+			this.SendMsgButton.Location = new System.Drawing.Point(12, 276);
+			this.SendMsgButton.Name = "SendMsgButton";
+			this.SendMsgButton.Size = new System.Drawing.Size(60, 23);
+			this.SendMsgButton.TabIndex = 7;
+			this.SendMsgButton.Text = "Send";
+			this.SendMsgButton.UseVisualStyleBackColor = true;
+			this.SendMsgButton.Click += new System.EventHandler(this.SendMsgButton_Click);
+			// 
+			// MsgBox
+			// 
+			this.MsgBox.Enabled = false;
+			this.MsgBox.Location = new System.Drawing.Point(78, 278);
+			this.MsgBox.Name = "MsgBox";
+			this.MsgBox.Size = new System.Drawing.Size(253, 20);
+			this.MsgBox.TabIndex = 8;
+			this.MsgBox.TextChanged += new System.EventHandler(this.MsgBox_TextChanged);
+			this.MsgBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MsgBox_KeyPress);
+			// 
+			// MessageList
+			// 
+			this.MessageList.Activation = System.Windows.Forms.ItemActivation.TwoClick;
+			this.MessageList.Enabled = false;
+			listViewItem1.Tag = "100600";
+			listViewItem2.Tag = "100601";
+			this.MessageList.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+			this.MessageList.Location = new System.Drawing.Point(337, 6);
+			this.MessageList.MultiSelect = false;
+			this.MessageList.Name = "MessageList";
+			this.MessageList.Size = new System.Drawing.Size(213, 292);
+			this.MessageList.TabIndex = 9;
+			this.MessageList.UseCompatibleStateImageBehavior = false;
+			this.MessageList.View = System.Windows.Forms.View.List;
+			this.MessageList.ItemActivate += new System.EventHandler(this.MessageList_ItemActivate);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(564, 314);
+			this.Controls.Add(this.MessageList);
+			this.Controls.Add(this.MsgBox);
+			this.Controls.Add(this.SendMsgButton);
+			this.Controls.Add(this.OutputLog);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.PortNoBox);
+			this.Controls.Add(this.ServerIpBox);
+			this.Controls.Add(this.CloseSocketButton);
+			this.Controls.Add(this.OpenSocketButton);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Socket Sender - Client";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button OpenSocketButton;
+		private System.Windows.Forms.Button CloseSocketButton;
+		private System.Windows.Forms.TextBox ServerIpBox;
+		private System.Windows.Forms.TextBox PortNoBox;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ListBox OutputLog;
+		private System.Windows.Forms.Button SendMsgButton;
+		private System.Windows.Forms.TextBox MsgBox;
+		private System.Windows.Forms.ListView MessageList;
+		private System.Windows.Forms.ToolTip toolTip1;
+	}
+}
+
---
+++ SocketSenderClient/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace SocketSenderClient
+{
+	public partial class Form1 : Form
+	{
+		private IProgress<string> progress_str;
+		private IProgress<Boolean> progress_hmi;
+
+		byte[] m_dataBuffer = new byte[10];
+		IAsyncResult m_result;
+		public AsyncCallback m_pfnCallBack;
+		private Socket m_clientSocket;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			progress_str = new Progress<string>(status =>
+			{
+				OutputLog.Items.Add(status);
+				OutputLog.TopIndex = OutputLog.Items.Count - 1;
+			});
+
+			progress_hmi = new Progress<Boolean>(status =>
+			{
+				ServerIpBox.Enabled = !status;
+				PortNoBox.Enabled = !status;
+				OpenSocketButton.Enabled = !status;
+				CloseSocketButton.Enabled = status;
+				if (status)
+				{
+					UpdateSendBtnStatus();
+				}
+				else
+				{
+					SendMsgButton.Enabled = status;
+				}
+				MsgBox.Enabled = status;
+				MessageList.Enabled = status;
+
+				if ((status == false) && (m_clientSocket != null))
+				{
+					m_clientSocket.Close();
+					m_clientSocket = null;
+				}
+			});
+
+			ServerIpBox.Text = GetIP();
+			PortNoBox.Text = "5050";
+
+			toolTip1.SetToolTip(MsgBox, "Hex input (e.g. DEADBEEF01)");
+		}
+
+		private string GetIP()
+		{
+			String strHostName = Dns.GetHostName();
+
+			// Find host by name
+			IPHostEntry iphostentry = Dns.GetHostEntry(strHostName);
+
+			// Grab the first IP addresses
+			String IPStr = "";
+			foreach (IPAddress ipaddress in iphostentry.AddressList)
+			{
+				IPStr = ipaddress.ToString();
+				return IPStr;
+			}
+			return IPStr;
+		}
+
+		public static byte[] StringToByteArray(string hex)
+		{
+			return Enumerable.Range(0, hex.Length)
+							 .Where(x => x % 2 == 0)
+							 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+							 .ToArray();
+		}
+
+		public void OnDataReceived(IAsyncResult asyn)
+		{
+			try
+			{
+				SocketPacket theSockId = (SocketPacket)asyn.AsyncState;
+				int iRx = theSockId.thisSocket.EndReceive(asyn);
+				char[] chars = new char[iRx + 1];
+				Decoder d = Encoding.UTF8.GetDecoder();
+				int charLen = d.GetChars(theSockId.dataBuffer, 0, iRx, chars, 0);
+				String szData = new String(chars);
+				progress_str.Report(szData);
+				WaitForData();
+			}
+			catch (ObjectDisposedException)
+			{
+				progress_str.Report("OnDataReceived: Socket has been closed");
+			}
+			catch (SocketException se)
+			{
+				progress_str.Report(se.Message);
+				progress_hmi.Report(false);
+			}
+		}
+
+		public void WaitForData()
+		{
+			try
+			{
+				if (m_pfnCallBack == null)
+				{
+					m_pfnCallBack = new AsyncCallback(OnDataReceived);
+				}
+				SocketPacket theSocPkt = new SocketPacket();
+				theSocPkt.thisSocket = m_clientSocket;
+				// Start listening to the data asynchronously
+				m_result = m_clientSocket.BeginReceive(theSocPkt.dataBuffer,
+														0,
+														theSocPkt.dataBuffer.Length,
+														SocketFlags.None,
+														m_pfnCallBack,
+														theSocPkt);
+			}
+			catch (SocketException se)
+			{
+				progress_str.Report(se.Message);
+			}
+		}
+
+		public class SocketPacket
+		{
+			public System.Net.Sockets.Socket thisSocket;
+			public byte[] dataBuffer = new byte[1];
+		}
+
+		private void OpenSocketButton_Click(object sender, EventArgs e)
+		{
+			OpenSocketButton.Enabled = false;
+			progress_str.Report("Opening Socket...");
+
+			// See if we have text on the IP and Port text fields
+			if (ServerIpBox.Text == "" || PortNoBox.Text == "")
+			{
+				progress_str.Report("IP Address and Port Number are required to connect to the Server");
+				progress_hmi.Report(false);
+				return;
+			}
+
+			try
+			{
+				// Create the socket instance
+				m_clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+				m_clientSocket.ReceiveTimeout = 15000;
+
+				// Cet the remote IP address
+				IPAddress ip = parseIpAddr();
+				int iPortNo = parsePortNo();
+
+				if ((ip != null) && (iPortNo >= System.Net.IPEndPoint.MinPort) && (iPortNo <= System.Net.IPEndPoint.MaxPort))
+				{
+					// Create the end point 
+					IPEndPoint ipEnd = new IPEndPoint(ip, iPortNo);
+					// Connect to the remote host
+					m_clientSocket.Connect(ipEnd);
+					if (m_clientSocket.Connected)
+					{
+
+						progress_hmi.Report(true);
+						//Wait for data asynchronously 
+						WaitForData();
+					}
+					else
+					{
+						progress_hmi.Report(false);
+					}
+				}
+				else
+				{
+					progress_hmi.Report(false);
+				}
+			}
+			catch (SocketException se)
+			{
+				progress_str.Report("Connection failed, is the server running?");
+				progress_str.Report(se.Message);
+				progress_hmi.Report(false);
+			}
+		}
+
+		private IPAddress parseIpAddr()
+		{
+			try
+			{
+				IPAddress ip = IPAddress.Parse(ServerIpBox.Text);
+				return ip;
+			}
+			catch(Exception e)
+			{
+				progress_str.Report("ERROR: Could not parse server IP!");
+				progress_str.Report(e.ToString());
+			}
+
+			return null;
+		}
+
+		private int parsePortNo()
+		{
+			int port = -1;
+
+			try
+			{
+				port = System.Convert.ToInt16(PortNoBox.Text);
+			}
+			catch (Exception e)
+			{
+				progress_str.Report("ERROR: Could not parse port number!");
+				progress_str.Report(e.ToString());
+			}
+
+			return port;
+		}
+
+		private void UpdateSendBtnStatus()
+		{
+			if(String.IsNullOrEmpty(MsgBox.Text))
+			{
+				SendMsgButton.Enabled = false;
+			}
+			else
+			{
+				SendMsgButton.Enabled = true;
+			}
+		}
+
+		private void CloseSocketButton_Click(object sender, EventArgs e)
+		{
+			progress_str.Report("Closing Socket..."); 
+			
+			progress_hmi.Report(false);
+		}
+
+		private void SendMsgButton_Click(object sender, EventArgs e)
+		{
+			progress_str.Report("Sending Msg...");
+
+			try
+			{
+				Object objData = MsgBox.Text;
+				byte[] byData = StringToByteArray(objData.ToString());
+				progress_str.Report(MsgBox.Text);
+				if (m_clientSocket != null)
+				{
+					m_clientSocket.Send(byData);
+				}
+			}
+			catch (SocketException se)
+			{
+				progress_str.Report(se.Message);
+			}	
+		}
+
+		private void PortNoBox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (!Regex.IsMatch(e.KeyChar.ToString(), "^[0-9\b]+$"))
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void MsgBox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			
+			if (!Regex.IsMatch(e.KeyChar.ToString(), "^[0-9a-fA-F\b]+$"))
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void MessageList_ItemActivate(object sender, EventArgs e)
+		{
+			MsgBox.Text = MessageList.SelectedItems[0].Tag.ToString();
+		}
+
+		private void MsgBox_TextChanged(object sender, EventArgs e)
+		{
+			UpdateSendBtnStatus();
+		}
+	}
+}
